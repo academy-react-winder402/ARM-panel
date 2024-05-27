ddbecae3c0a1b20d0fad0e305bbd6bf0d0c0c3be
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-05-27 10:57:51 -0700

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,30 +14,44 @@ import StatsVertical from "../@core/components/Stats Vertical/StatsVertical";
 import { User, FileText, File } from "react-feather";
 import SupportTracker from "../@core/components/Support Tracker/SupportTracker.js";
 import SupportTracker2 from "../@core/components/Support Tracker/SupportTracker2.js";
-import { ThemeColors } from '@src/utility/context/ThemeColors';
+import { ThemeColors } from "@src/utility/context/ThemeColors";
 
 const Home = () => {
-  const { colors } = useContext(ThemeColors)
+  const { colors } = useContext(ThemeColors);
   return (
     <div>
       <Row>
-        <Col lg='6' sm='12'>
+        <Col lg="6" sm="12">
           <CardCongratulations />
         </Col>
-        <Col xl='3' md='4' sm='6'>
-          <StatsVertical icon={<User />} stats={26} statTitle={"تعداد تمام کاربرها"}/>
+        <Col xl="3" md="4" sm="6">
+          <StatsVertical
+            icon={<User />}
+            stats={26}
+            statTitle={"تعداد تمام کاربرها"}
+          />
         </Col>
-        <Col xl='3' md='4' sm='6'>
-          <StatsVertical icon={<File />} stats={1} statTitle={"تعداد تمام دوره های رزرو شده"}/>
+        <Col xl="3" md="4" sm="6">
+          <StatsVertical
+            icon={<File />}
+            stats={1}
+            statTitle={"تعداد تمام دوره های رزرو شده"}
+          />
         </Col>
       </Row>
 
       <Row>
-        <Col lg='6' xs='12'>
-          <SupportTracker primary={colors.primary.main} danger={colors.danger.main} />
+        <Col lg="6" xs="12">
+          <SupportTracker
+            primary={colors.primary.main}
+            danger={colors.danger.main}
+          />
         </Col>
-        <Col lg='6' xs='12'>
-          <SupportTracker2 primary={colors.primary.main} danger={colors.danger.main} />
+        <Col lg="6" xs="12">
+          <SupportTracker2
+            primary={colors.primary.main}
+            danger={colors.danger.main}
+          />
         </Col>
       </Row>
     </div>
